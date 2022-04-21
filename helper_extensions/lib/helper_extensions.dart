65bc5c1ad3a94705c7a908af89fbfb747971f491
sawthiha <sawthiha@mtu.edu.mm>
2022-04-21 14:09:06 +0900

    paint extension inited! copyWith implemented!

--- helper_extensions/lib/helper_extensions.dart
+++ helper_extensions/lib/helper_extensions.dart
@@ -1,3 +1,30 @@
 library helper_extensions;
 
+import 'dart:ui';
 
+extension PaintExtension on Paint  {
+  Paint copyWith(
+    {
+      Color? color,
+      double? strokeWidth,
+      StrokeCap? strokeCap,
+      StrokeJoin? strokeJoin,
+      PaintingStyle? style,
+      MaskFilter? maskFilter,
+      Shader? shader,
+      ColorFilter? colorFilter,
+      BlendMode? blendMode,
+      bool? isAntiAlias,
+    }
+  ) => Paint()
+    ..color = color ?? this.color
+    ..strokeWidth = strokeWidth ?? this.strokeWidth
+    ..strokeCap = strokeCap ?? this.strokeCap
+    ..style = style ?? this.style
+    ..strokeJoin = strokeJoin ?? this.strokeJoin
+    ..maskFilter = maskFilter ?? this.maskFilter
+    ..shader = shader ?? this.shader
+    ..colorFilter = colorFilter ?? this.colorFilter
+    ..blendMode = blendMode ?? this.blendMode
+    ..isAntiAlias = isAntiAlias ?? this.isAntiAlias;
+}
