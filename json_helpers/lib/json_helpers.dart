450e89f9b0a698ba1be8c52d468d458e080e5023
sawthiha <sawthiha@mtu.edu.mm>
2022-05-25 23:46:23 +0900

    Helper Extension dependencies exposed!

--- json_helpers/lib/json_helpers.dart
+++ json_helpers/lib/json_helpers.dart
@@ -4,4 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:helper_extensions/helper_extensions.dart';
 import 'package:json_annotation/json_annotation.dart';
 
+export 'package:helper_extensions/helper_extensions.dart';
+
 part 'src/converters.dart';
