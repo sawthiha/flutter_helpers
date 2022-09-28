d329a0570001e99cd6c951ab2d79283063a20c5c
sawthiha <sawthiha@mtu.edu.mm>
2022-09-28 19:28:41 +0900

    Added forgotten import!

--- widget_helpers/lib/widget_helpers.dart
+++ widget_helpers/lib/widget_helpers.dart
@@ -3,6 +3,7 @@ library widget_helpers;
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:sensors_plus/sensors_plus.dart';
