4a0e2a350dc6717feb14ee9ef72052cf7f77345f
sawthiha <sawthiha@mtu.edu.mm>
2022-11-14 18:14:24 +0900

    ToggleButton and ToggleButtonGroup added!

--- widget_helpers/lib/giz/buttons.dart
+++ widget_helpers/lib/giz/buttons.dart
@@ -43,3 +43,75 @@ class GizFloatingButton extends StatelessWidget {
   );
 }
 
+class ToggleButtonGroupItem<T>  {
+
+  final T value;
+  final Widget child;
+
+  ToggleButtonGroupItem({
+    required this.value,
+    required this.child,
+  });
+
+}
+
+class ToggleButtonGroup<T> extends StatelessWidget   {
+
+  final double spacing;
+  final Size itemSize;
+  final T groupValue;
+  final ValueChanged<T> onChanged;
+  final Iterable<ToggleButtonGroupItem<T>> items;
+
+  const ToggleButtonGroup({super.key, required this.groupValue, required this.onChanged, required this.items, required this.itemSize, required this.spacing});
+
+  @override
+  Widget build(BuildContext context) => Wrap(
+    spacing: spacing,
+    children: [
+      for(var item in items)
+        ToggleButton(
+          size: itemSize,
+          isToggled: groupValue == item.value,
+          child: item.child,
+          onTap: ()  {
+            onChanged(item.value);
+          },
+        ),
+    ],
+  );
+
+}
+
+class ToggleButton extends StatelessWidget  {
+
+  final bool isToggled;
+  final Widget child;
+  final Size size;
+  final VoidCallback onTap;
+
+  const ToggleButton({super.key,
+    required this.size,
+    required this.isToggled,
+    required this.child,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) => Material(
+    color: Colors.transparent,
+    child: InkWell(
+      onTap: onTap,
+      child: SizedBox.fromSize(
+        size: size,
+        child: Container(
+          color: isToggled ? GizColors.primaryOpacity30: Colors.transparent,
+          child: Center(
+            child: child,
+          ),
+        ),
+      ),
+    ),
+  );
+
+}
